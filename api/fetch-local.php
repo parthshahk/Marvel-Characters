111ee3dacca5a430d695425c11fe19a84248d74d
Parth Shah <parthshahk@gmail.com>
2018-10-28 21:38:37 +0530

    .

--- api/fetch-local.php
+++ api/fetch-local.php
@@ -2,8 +2,7 @@
 
     $type = $_GET['type'];
 
-    $con=mysqli_connect("localhost","parth","parth","marvel");
-    // $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
+    include 'connection.php';
 
     if($type == 'like'){
 
